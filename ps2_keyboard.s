afb85b1c62a6ff2f61ae183f93ba30cb734e3700
Carben-dev <32303575+Carben-dev@users.noreply.github.com>
2018-11-20 14:23:00 -0500

    Update ps2_keyboard.s
    Fix text space problem

--- ps2_keyboard.s
+++ ps2_keyboard.s
@@ -7,44 +7,44 @@
 
 .global _start
 _start:
-	# Setup SP
+    # Setup SP
     movia sp, 0x0000FFF0
 
-	# Put PS2 Data and Control addr in reg
+    # Put PS2 Data and Control addr in reg
     movia r16, PS2_DATA_1
     movia r17, PS2_CTL_1
 
-	# Setup PS2 control interrupt
+    # Setup PS2 control interrupt
     # Setup the first bit of PS2 CTL to 1 to enable PS2 Read interrupt,
     # PS2 Controller will trigger interrupt on data received.
-    movi r8, 0b1
+    movi  r8, 0b1
     stwio r8, 0(r17)
 
     # Enable IRQ 7 for PS2 controller 1
     # PS2 controller 1 use IRQ 7 to request interrupt.
-    movi r8, 0b10000000
+    movi  r8, 0b10000000
     wrctl ienable, r8
 
     # Enable interrupt on CPU
-    movi r8, 0b1
+    movi  r8, 0b1
     wrctl status, r8
 
     # Loop wait for interrupt
 loop:
-	br loop
+    br loop
 
 # Interrupt handler
 .section .exceptions, "ax"
 interrupt_handler:
-	# PL
+    # PL
     addi sp, sp, -8
     stw r8, 0(sp)
     stw r9, 4(sp)
 
-	# which device caused the interrupt?
-   	rdctl et, ctl4                      # read out iPending
-    andi et, et, 0b10000000             # Mask reading
-    beq et, r0, exit_interrupt_handler  # If not cause by PS2, exit
+    # which device caused the interrupt?
+    rdctl et, ctl4                      # read out iPending
+    andi  et, et, 0b10000000             # Mask reading
+    beq   et, r0, exit_interrupt_handler  # If not cause by PS2, exit
 
 read_data:
     # handle interrupt
@@ -55,7 +55,7 @@ read_data:
 
     # Check if data vaild
     andi et, r8, 0b1000000000000000
-    beq et, r0, read_data
+    beq  et, r0, read_data
 
     # If vaild, mask it and save it to r9
     andi r9, r8, 0b11111111             # Real Data now in r9
@@ -92,10 +92,10 @@ key_press:
     # Once data been read, PS2 automatically ack interrupt in handled
 
 exit_interrupt_handler:
-	# EPL
-	ldw r9, 4(sp)
+    # EPL
+    ldw r9, 4(sp)
     ldw r8, 0(sp)
     addi sp, sp, 8
 
-	subi ea, ea, 4
+    subi ea, ea, 4
     eret
