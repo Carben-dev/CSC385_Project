ef8b09ffa7d396271e59a9bec1e17741eefe9d1d
Jacob Peng <jacob.peng@hotmail.com>
2018-12-04 15:54:26 -0500

    Adding code for video streaming

--- interrupt_example.s
+++ interrupt_example.s
@@ -14,20 +14,22 @@
 
 .text        # executable code follows
 .global     _start
+.equ        ADDR_Front_buffer, 0xFF203020
+.equ        ADDR_Video_in_controller, 0xFF20306C
 _start:
 /* set up the stack */
         movia   sp, SDRAM_END - 3   # stack starts from largest memory
                                     # address
-/*  enable video streaming */
-        /*movia r8, 0xFF20306C
-        movi  r9, 0b100
 
-        stwio r9, 0(r8)
+        # Setup Video Steam
+        movia r8,ADDR_Video_in_controller
+        movi  r9,0x4          # When bit 2 of 0xFF20306C is set the video from the video in source is stored into the pixel buffer
+        stwio r9,0(r8)        # Write the value
 
-        movia r8, PIXEL_BUF_CTRL_BASE
-        movi  r9, 0b1
-        stwio r9, 0(r8)
-        */
+        movia r8,ADDR_Front_buffer
+        movi  r9,0x1          # When 1 is written to this register then every 1/60 the data from the back
+                          # register is swapped into this register.
+        stwio r9,0(r8)        # Write the value
 
         /* set up the lego controller */
         movia   r15, JP2_BASE
